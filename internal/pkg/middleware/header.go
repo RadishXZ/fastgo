6ec1b379d7ba9d73a80f3e07ace83fed91f112ed
zls1998 <1404209664@qq.com>
2026-02-08 11:02:57 +0800

    应用假如优雅关停

--- internal/pkg/middleware/header.go
+++ internal/pkg/middleware/header.go
@@ -9,6 +9,8 @@ import (
 
 // NoCache 是一个 Gin 的中间件, 用来禁止客户端缓存 HTTP 请求的返回结果.
 func NoCache(c *gin.Context) {
+
+	// 告诉浏览器 "这个接口的数据经常变, 不存缓存, 需要每次都得找服务器要最新的请求"
 	c.Header("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate, value")
 	
 	// 设置HTTP 的 Expires 响应头告诉客户端/中间缓存资源在什么时候过期, 然后过期后需要重新向服务器获取内容
@@ -17,14 +19,22 @@ func NoCache(c *gin.Context) {
 
 	// Last-Modified: 可选，表示资源最后修改时间（用于协商缓存）
 	c.Header("Last-Modified", time.Now().UTC().Format(http.TimeFormat))
+
+	// 把控制权交给下一个中间件或者最终处理函数
 	c.Next()
 }
 
-// Cors 是一个Gin的中间件, 用来设置options 请求的返回头, 然后退出中间件链, 并结束请求(浏览器跨域设置)
+// Cors 是一个Gin的中间件, 用来作为安全机制, 因为默认情况下, 浏览器不允许 A 网站前端代码去请求 B 网站的后端接口
+// Cors 就是后端告诉浏览器: 我是允许 A 网站来访问我的
 // 实现一个返回头的设置
 func Cors(c *gin.Context) {
+
+	// 如果是正常的请求, 就会往下走 c.Next()
 	if c.Request.Method != "OPTIONS" {
 		c.Next()
+	
+	// 如果是 OPTIONS 请求, 则直接在 Header 里面告诉允许哪些方法, 允许哪些头部信息(GET\POST\PUT ...), 最后通过 c.AborWithStatus 结束
+	// 这其实是浏览器的探路请求, 在正式发送数据前, 浏览器会先问一句: 我能连你么
 	} else {
 		// 允许任意来源访问 "*" 则表示不需要携带凭证
 		c.Header("Access-Control-Allow-Origin", "*")
--- internal/pkg/middleware/requestid.go
+++ internal/pkg/middleware/requestid.go
@@ -8,18 +8,29 @@ import (
 	"github.com/RadishXZ/fastgo/internal/pkg/known"
 )
 
-// RequestID 是一个 Gin 中间件, 用来在每一个 HTTP 请求的 context, response 中注入 `x-request-id` 键值对
+// RequestID 是一个 Gin 中间件, 目的是给每一个 HTTP 请求都发放一个 RequestID
+// 作用主要是如果系统报错了, 我们只需要去日志里搜这个 ID 就能看到这个请求从进来到出去发生的所有事情
+// RequestID 用来在每一个 HTTP 请求的 context, response 中注入 `x-request-id` 键值对
 func RequestID() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// 从请求头中获取 `x-request-id` , 如果不存在的话就获取新的 uuid
+		// 从请求头中获取 `x-request-id` , 看看请求头里面有没有 x-request-id , 如果不存在的话就获取一个新的 uuid
 		requestID := c.Request.Header.Get(known.XRequestID)
 		
 		if requestID == "" {
 			requestID = uuid.New().String()
 		}
 
-		// 将 RequestID 保存到 cntext.Context 中, 以便后续程序使用
+		// 生成一个新的 Context , 并且将 RequestID 保存到 context.Context 中, 以便后续程序使用
+		// c.Request.Context 拿到当前原本的上下文
+		// contextx.WitchRequestID 跳过了原本的c.Request.Context 这个空请求
+		// 整个动作流程: 跳过了原本的空背包, 往里面放一个键值对 Key 是 requestIDKey , Value 是uuid
+		// 结果: 返回了一个新的 context(ctx) , 注意: Go 的 Context 是不可变的, 所以想要修改它, 只能生成一个新的
 		ctx := contextx.WithRequestID(c.Request.Context(), requestID)
+
+		// 把这个新的 Context 塞入回 HTTP 请求里, 替换掉 HTTP 请求中那个旧的空背包
+		// c.Request 对象是指 Go 标准库的 http.Request 结构体
+		// WithContext 相当于是拿出了一个新的单(Request_new), 把旧的单(c.Request)上URL、Header、Body 指针等所有信息(浅拷贝)抄过来
+		// 替换 Context 的时候, 不会抄旧的 Context_A 这一个, 而是把刚才第一步生成的 Context_B 填进去
 		c.Request = c.Request.WithContext(ctx)
 
 		// 将 RequestID 保存到 HTTP 返回头中, Header 的键为 `x-request-id`
