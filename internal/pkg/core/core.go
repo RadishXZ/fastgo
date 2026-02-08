fb38d3d810d7238f94dc6a66f9f634140ac0c1ac
zls1998 <1404209664@qq.com>
2026-02-08 15:22:29 +0800

    实现统一错误码返回

--- internal/pkg/core/core.go
+++ internal/pkg/core/core.go
@@ -7,14 +7,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// ErrorResponse 定义了错误响应的结构
+// 用于 API 请求中发生错误时返回统一的格式化错误信息
 type ErrorResponse struct {
+	// 错误原因, 表示错误类型
 	Reason string `json:"reason,omitempty"`
+
+	// 错误详情的描述信息
 	Message string `json:"message,omitempty"`
 }
 
+// WriteResponse 是通用的响应函数
+// 它会根据是否发生错误, 生成成功响应或标准化的错误响应
 func WriteResponse(c *gin.Context, err error, data any) {
+
+	// 如果发生错误, 生成错误响应
 	if err != nil {
-		errx := errorsx.FormError(err)
+		errx := errorsx.FormError(err) // 提取错误详细信息
 		c.JSON(errx.Code, ErrorResponse{
 			Reason: errx.Reason,
 			Message: errx.Message,
@@ -22,5 +31,6 @@ func WriteResponse(c *gin.Context, err error, data any) {
 		return
 	}
 
+	// 如果没有错误, 返回成功响应
 	c.JSON(http.StatusOK, data)
 }
--- internal/pkg/errorsx/errorsx.go
+++ internal/pkg/errorsx/errorsx.go
@@ -5,8 +5,9 @@ import (
 	"fmt"
 )
 
+// 创建一个针对统一错误码标准格式的结构体
 type ErrorX struct {
-	// Code 表示错误的 HTTP 状态码, 用于与客户端进行交互时标识错误的类型
+	// Code 表示错误的 HTTP 状态码(200\400\500等), 用于与客户端进行交互时标识错误的类型
 	Code int `json:"code,omitempty"`
 
 	// Reason 表示错误发生的原因, 通常为业务错误码, 用于精准定位问题
@@ -25,13 +26,15 @@ func New(code int, reason string, format string, args ...any) *ErrorX {
 	}
 }
 
-// Error 实现 error 接口中的 `Error` 方法
+// Error 实现 error 接口中的 `Error` 方法, 使用 Error() string , 可以完美融入 Go 的原生错误体系
 func (err *ErrorX) Error() string {
 	return fmt.Sprintf("error: code = %d reason = %s message = %s", err.Code, err.Reason, err.Message)
 }
 
-// WithMessage 设置错误的 Message 字段
+// 在 WithMessage 中, 为 ErrorX.Message 字段中填入错误的 Message 字段
 func (err *ErrorX) WithMessage(format string, args ...any) *ErrorX {
+
+	// 将原有的 Code 和 Reason 拷贝过去, 并通过 fmt.Sprintf 格式化传入的字符串, 并存入副本的 Message
 	err.Message = fmt.Sprintf(format, args...)
 	return err
 }
